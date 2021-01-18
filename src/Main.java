3ffd2bd76335059bbfb436147fb2e6bb69e3cb8b
Yusuf Guner <yusuf_guner@msn.com>
2021-01-18 11:13:57 +0100

    Création de la classe statique Monde

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(personnageFactory());
+		//System.out.println(personnageFactory());
 	}
 
-	
-	/**
-	 * Créer un personnage avec tous ses attributs. 
-	 * Demande a l'utilisateur d'entrer le nom du personnage.
-	 * retour: une instance de la classe Personnage correctement instancié.
-	 * @return 
-	 */
-	public static Personnage personnageFactory(){
-	    // Demander a l'utilisateur un nom de personnage
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Definir le nom du joueur: ");
-		String nom = scanner.nextLine();
-		int degat = 20;
-		int pointDeVie = 30;
-	    // Creer un nouveau personnage en utilisant le constructeur avec tous ses params (dont le nom qui vient d'être choisi par l'utilisateur)
-		Personnage personnage = new Personnage(pointDeVie, degat, nom);
-	    // Retourner l'instance du personnage
-		return personnage;
-	}
 }
---
+++ src/Monde.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Monde {
+	
+	/**
+	 * Créer un personnage avec tous ses attributs. 
+	 * Demande a l'utilisateur d'entrer le nom du personnage.
+	 * retour: une instance de la classe Personnage correctement instancié.
+	 * @return le personnage crée
+	 */
+	public static Personnage personnageFactory(){
+	    // Demander a l'utilisateur un nom de personnage
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Definir le nom du personnage: ");
+		String nom = scanner.nextLine();
+		int degat = 20;
+		int pointDeVie = 30;
+	    // Creer un nouveau personnage en utilisant le constructeur avec tous ses params (dont le nom qui vient d'être choisi par l'utilisateur)
+		Personnage personnage = new Personnage(pointDeVie, degat, nom);
+	    // Retourner l'instance du personnage
+		return personnage;
+	}
+}
