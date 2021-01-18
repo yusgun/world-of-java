bffd544fb163ab2acbe2ab266ea90d27f1e0c31b
Yusuf Guner <yusuf_guner@msn.com>
2021-01-18 11:22:11 +0100

    Creation de la classe Monstre

---
+++ src/Monstre.java
@@ -0,0 +1,24 @@
+/**
+ * Classe représentant un Monstre
+ * @author yusuf
+ */
+public class Monstre extends Personnage {
+
+	/**
+	 * Constructeur vide
+	 */
+	public Monstre() {
+		super();
+	}
+
+	/**
+	 * Constructeur avec les paramètres de la superclasse
+	 * @param pointDeVie - Point de vie du Monstre
+	 * @param degat - Point de dégat du Monstre
+	 * @param nom - Nom du Monstre
+	 */
+	public Monstre(int pointDeVie, int degat, String nom) {
+		super(pointDeVie, degat, nom);
+	}
+	
+}
